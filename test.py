3c583b696820da376be4a8890ab6ae0ab75f1ac5
rICHi97 <947133225@qq.com>
2021-10-22 02:16:03 +0800

    回归矫正
    对端子分组，对于每组端子，个数少的采用原始数据；个数多的基于回归矫正，高度方向倾斜角取平均值

--- test.py
+++ test.py
@@ -15,11 +15,7 @@ import visualization
 start = time.process_time()
 
 recs_xy_list = recdata_io.RecDataIO.read_rec_txt('2_original.txt')
-reg_shape_data = recdata_correcting.Regression.regression(
-	recs_xy_list,
-	independent=['center'],
-	dependent=['length_H', 'length_W', 'rotate_angle_W']
-)
+test_reg_shape_data = recdata_correcting.Correction.correct_rec(recs_xy_list)
 
 end = time.process_time()
 print(end - start)
